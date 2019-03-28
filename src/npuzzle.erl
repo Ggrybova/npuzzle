76915283074257b445329b64ab81e586d44216aa
ggrybova <g.grybova@betinvest.com>
2019-03-28 20:29:07 +0200

    define size of map

--- src/npuzzle.erl
+++ src/npuzzle.erl
@@ -40,19 +40,56 @@ get_input_data(FileName) ->
     case file:open("input_files/"++FileName, read) of
         {ok, File} ->
             case read_map(File) of
-                {ok, Data} -> io:format("Data: ~p~n", [Data]);
+                {ok, Data} -> io:format("Data: ~p~n", [dict:to_list(Data)]);
                 Error -> Error
             end;
         Error -> Error
     end.
 
 read_map(File) ->
-    read_map(File, []).
-
-read_map(File, Acc) ->
-    case io:get_line(File, '') of
-        eof -> Acc;
-        Line ->
-            io:format("Line: ~p~n", [Line]),
-            read_map(File, [Line | Acc])
+    case get_size(File) of
+        {ok, Size} when is_integer(Size) andalso Size > 2 ->
+            io:format("     Size: ~p~n", [Size]),
+            read_map(File, Size, dict:store(size, Size, dict:new()));
+        _ -> {error, "Wrong puzzle size"}
     end.
+
+read_map(File, Size, State) ->
+    case io:get_line(File, '') of
+        eof -> {ok, State};
+        Line ->
+            case string:str(Line, "#") of
+                0 ->
+                    fill_state(lists:droplast(Line), Size, State),
+                    read_map(File, Size, State);
+                N when is_integer(N) andalso N > 1 ->
+                    fill_state(string:left(Line, N-1), Size, State),
+                    read_map(File, Size, State);
+                _ ->
+                    read_map(File, Size, State)
+            end
+    end.
+
+get_size(File) ->
+    case io:get_line(File, '') of
+        eof -> error;
+        Line ->
+            case string:str(Line, "#") of
+                0 ->
+                    case string:words(Line) of
+                        1 ->
+                            {S, _} = string:to_integer(Line),
+                            {ok, S};
+                        _ ->
+                            error
+                    end;
+                N when is_integer(N) andalso N > 0 ->
+                    get_size(File);
+                _ ->
+                    error
+            end
+    end.
+
+fill_state(Line, Size, State) ->
+    io:format("State: ~p~n", [dict:to_list(State)]).
+
